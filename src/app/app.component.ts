5610b969d3bf4d961c836b2c70ecc6624a5f24ec
JulianChu <yulang.chu@gmail.com>
2017-04-19 10:51:17 +0800

    Event binding for enter event, two-way binding for todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  inputHint:string = "What needs to be done?"
+  inputHint: string = "What needs to be done?"
+  todos: any[] = [];
+  todo: string;
+  
+  AddItemToTodosArray($event: KeyboardEvent): void {
+    if ($event.keyCode === 13) {
+      this.todo = ($event.target as HTMLInputElement).value;
+      this.todos.push(this.todo);
+      this.todo = '';
+    }
+
+  }
 }
